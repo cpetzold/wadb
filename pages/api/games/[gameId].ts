91d5af9eb96b6e693e285ebe26f2701913970b5a
Conner Petzold <cpetzold@gmail.com>
2021-11-30 23:46:50 -0800

    fix

--- pages/api/games/[gameId].ts
+++ pages/api/games/[gameId].ts
@@ -1,19 +1,11 @@
-import { Document, Filter, ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
+
 import nc from "next-connect";
-import * as db from "../../../lib/db";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
 handler.get(async (req, res) => {
-  const games = await db.collection("games");
-  const gameId = req.query.gameId as string;
-  const conditions: Filter<Document>[] = [{ filename: gameId }];
-  if (ObjectId.isValid(gameId)) {
-    conditions.push({ _id: new ObjectId(gameId) });
-  }
-  const result = await games.findOne({ $or: conditions });
-  res.json(result);
+  res.send(200);
 });
 
 export default handler;
